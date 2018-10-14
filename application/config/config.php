8c3c7509cb573e7509efe67d6a2068405640ccf2
dugage <info@abintesa.es>
2018-10-14 12:39:51 +0200

    cambios en database y adaptación imaltel

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://www.multivoz.es/intranet/';
-//$config['base_url'] = 'http://localhost/ofertel/';
+//$config['base_url'] = 'http://www.imaltel.es/intranet/';
+$config['base_url'] = 'http://localhost/imaltel/';
 
 /*
 |--------------------------------------------------------------------------
